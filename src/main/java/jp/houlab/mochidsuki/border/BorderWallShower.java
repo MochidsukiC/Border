e6b0711c2acf2e5a3941fcbcb9dea84fdbb335ad
BuildTools <unconfigured@null.spigotmc.org>
2025-05-17 17:33:48 +0900

    from Mochidsuki Mini PC fix Border Showing Issue (Wall End Parts Invisible Issue)

--- src/main/java/jp/houlab/mochidsuki/border/BorderWallShower.java
+++ src/main/java/jp/houlab/mochidsuki/border/BorderWallShower.java
@@ -1,6 +1,5 @@
 package jp.houlab.mochidsuki.border;
 
-import com.comphenix.protocol.wrappers.WrappedBlockData;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -8,13 +7,9 @@ import org.bukkit.entity.*;
 import org.bukkit.scheduler.BukkitRunnable;
 
 
-import com.comphenix.protocol.wrappers.*;
 import org.bukkit.util.Transformation;
-import org.joml.Vector3f;
-import java.util.*;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 import static jp.houlab.mochidsuki.border.Main.plugin;
 import static jp.houlab.mochidsuki.border.Main.world;
@@ -25,10 +20,17 @@ import static jp.houlab.mochidsuki.border.Main.world;
  */
 public class BorderWallShower extends BukkitRunnable {
 
-    public List<BlockDisplay> px = new ArrayList<>();
-    public List<BlockDisplay> mx = new ArrayList<>();
-    public List<BlockDisplay> pz = new ArrayList<>();
-    public List<BlockDisplay> mz = new ArrayList<>();
+    //p - プラス m - マイナス
+    //x,z - 軸
+    //b - 戻しディスプレイ。blockDisplay1の格納先
+    public List<List<BlockDisplay>> px = new ArrayList<>();
+    public List<List<BlockDisplay>> pxb = new ArrayList<>();
+    public List<List<BlockDisplay>> mx = new ArrayList<>();
+    public List<List<BlockDisplay>> mxb = new ArrayList<>();
+    public List<List<BlockDisplay>> pz = new ArrayList<>();
+    public List<List<BlockDisplay>> pzb = new ArrayList<>();
+    public List<List<BlockDisplay>> mz = new ArrayList<>();
+    public List<List<BlockDisplay>> mzb = new ArrayList<>();
 
     public void setHeight(int height) {
         this.height = height;
@@ -46,10 +48,10 @@ public class BorderWallShower extends BukkitRunnable {
      */
     @Override
     public void run() {
-        controlWall(BorderInfo.getNowPX(),BorderInfo.getNowMZ(),BorderInfo.getNowPZ(),"x",px);
-        controlWall(BorderInfo.getNowMX(),BorderInfo.getNowMZ(),BorderInfo.getNowPZ(),"x",mx);
-        controlWall(BorderInfo.getNowPZ(),BorderInfo.getNowMX(),BorderInfo.getNowPX(),"z",pz);
-        controlWall(BorderInfo.getNowMZ(),BorderInfo.getNowMX(),BorderInfo.getNowPX(),"z",mz);
+        controlWall(BorderInfo.getNowPX(),BorderInfo.getNowMZ(),BorderInfo.getNowPZ(),"x",px,pxb);
+        controlWall(BorderInfo.getNowMX(),BorderInfo.getNowMZ(),BorderInfo.getNowPZ(),"x",mx,mxb);
+        controlWall(BorderInfo.getNowPZ(),BorderInfo.getNowMX(),BorderInfo.getNowPX(),"z",pz,pzb);
+        controlWall(BorderInfo.getNowMZ(),BorderInfo.getNowMX(),BorderInfo.getNowPX(),"z",mz,mzb);
 
         /*
         Issueメモ
@@ -67,135 +69,141 @@ public class BorderWallShower extends BukkitRunnable {
      * @param width 横幅のマイナス方向の座標
      * @param widthTop 横幅のプラス方向の座標
      * @param shaftCode 軸の方向を指定
-     * @param blockDisplays ブロックティスプレイを保存し、次の処理に渡すリスト
+     * @param blockDisplaysList ブロックティスプレイを保存し、次の処理に渡すリスト
      */
-    private void controlWall(final double shaft,double width,double widthTop,String shaftCode,List<BlockDisplay> blockDisplays) {
-        final int longK = 158; //1ブロックディスプレイの長さ
+    private void controlWall(final double shaft,double width,double widthTop,String shaftCode,List<List<BlockDisplay>> blockDisplaysList,List<List<BlockDisplay>> blockDisplaysList1) {
+        final int longK = 80; //1ブロックディスプレイの長さ
+        final int heightK = 40;
         double widthNow = width;
         int i = 0;
 
         for(;widthNow < widthTop;i++) {
-            if(i*2 < blockDisplays.size()) {
-                BlockDisplay blockDisplay = blockDisplays.get(i*2);
-                BlockDisplay blockDisplay1 = blockDisplays.get(i*2+1);
-                double x =0;
-                double z =0;
-                double sx =0;
-                double sz =0;
+            if(i < blockDisplaysList.size()) {
+                List<BlockDisplay> blockDisplays = blockDisplaysList.get(i);
+                List<BlockDisplay> blockDisplays1 = blockDisplaysList1.get(i);
+                for(int j = 0; j < blockDisplays.size(); j++) {
+                    BlockDisplay blockDisplay = blockDisplays.get(j);
+                    BlockDisplay blockDisplay1 = blockDisplays1.get(j);
 
-                double x1 =0;
-                double z1 =0;
-                double sx1 =0;
-                double sz1 =0;
+                    double x =0;
+                    double z =0;
+                    double sx =0;
+                    double sz =0;
 
-                if(shaftCode.equals("x")) {
-                    x = shaft;
-                    z = widthNow;
-                }else if(shaftCode.equals("z")) {
-                    x = widthNow;
-                    z = shaft;
+                    double x1 =0;
+                    double z1 =0;
+                    double sx1 =0;
+                    double sz1 =0;
+
+                    if(shaftCode.equals("x")) {
+                        x = shaft;
+                        z = widthNow;
+                    }else if(shaftCode.equals("z")) {
+                        x = widthNow;
+                        z = shaft;
+                    }
+                    Transformation transformation = blockDisplay.getTransformation();
+                    if (shaftCode.equals("x")) {
+                        sx = 1;
+                        sz = longK;
+                    } else if (shaftCode.equals("z")) {
+                        sx = longK;
+                        sz = 1;
+                    }
+
+
+                    widthNow = widthNow+longK;
+
+                    if(widthNow < widthTop) {
+
+                        if(shaftCode.equals("x")) {
+                            x1 = shaft;
+                            z1 = widthNow;
+                        }else if(shaftCode.equals("z")) {
+                            x1 = widthNow;
+                            z1 = shaft;
+                        }
+
+
+                        if(shaftCode.equals("x")) {
+                            sx1 = 1;
+                            sz1 = longK*-1;
+                        }else if(shaftCode.equals("z")) {
+                            sx1 = longK*-1;
+                            sz1 = 1;
+                        }
+
+                    }else {//末端処理
+
+                        if(shaftCode.equals("x")) {
+                            x1 = shaft;
+                            z1 = widthTop;
+                        }else if(shaftCode.equals("z")) {
+                            x1 = widthTop;
+                            z1 = shaft;
+                        }
+
+                        if(shaftCode.equals("x")) {
+                            sx1 = 1;
+                            sz1 = (widthTop - widthNow + longK)*-1;
+                        }else if(shaftCode.equals("z")) {
+                            sx1 = (widthTop - widthNow + longK)*-1;
+                            sz1 = 1;
+                        }
+
+                        if(shaftCode.equals("x")) {
+                            sz = (widthTop - widthNow + longK);
+                        }else if(shaftCode.equals("z")) {
+                            sx = (widthTop - widthNow + longK);
+                        }
+                    }
+
+                    widthNow = widthNow-longK;
+
+                    Transformation transformation1 = blockDisplay1.getTransformation();
+
+                    blockDisplay.getChunk().addPluginChunkTicket(plugin);
+                    blockDisplay1.getChunk().addPluginChunkTicket(plugin);
+                    new Location(world, x, -70+j*heightK, z).getChunk().addPluginChunkTicket(plugin);
+                    new Location(world, x1, -70+j*heightK, z1).getChunk().addPluginChunkTicket(plugin);
+
+
+                    blockDisplay.teleport(new Location(world, x, -70+j*heightK, z));
+                    blockDisplay1.teleport(new Location(world, x1, -70+j*heightK, z1));
+
+                    transformation.getScale().set(sx,-1*heightK,sz);
+                    blockDisplay.setTransformation(transformation);
+
+                    transformation1.getScale().set(sx1,-1*heightK,sz1);
+                    blockDisplay1.setTransformation(transformation1);
+
                 }
-                Transformation transformation = blockDisplay.getTransformation();
-                if (shaftCode.equals("x")) {
-                    sx = 1;
-                    sz = longK;
-                } else if (shaftCode.equals("z")) {
-                    sx = longK;
-                    sz = 1;
-                }
-
-
                 widthNow = widthNow+longK;
-
-                if(widthNow < widthTop) {
-
-                    if(shaftCode.equals("x")) {
-                        x1 = shaft;
-                        z1 = widthNow;
-                    }else if(shaftCode.equals("z")) {
-                        x1 = widthNow;
-                        z1 = shaft;
-                    }
-
-
-                    if(shaftCode.equals("x")) {
-                        sx1 = 1;
-                        sz1 = longK*-1;
-                    }else if(shaftCode.equals("z")) {
-                        sx1 = longK*-1;
-                        sz1 = 1;
-                    }
-
-                }else {//末端処理
-
-                    if(shaftCode.equals("x")) {
-                        x1 = shaft;
-                        z1 = widthTop;
-                    }else if(shaftCode.equals("z")) {
-                        x1 = widthTop;
-                        z1 = shaft;
-                    }
-
-                    if(shaftCode.equals("x")) {
-                        sx1 = 1;
-                        sz1 = (widthTop - widthNow + longK)*-1;
-                    }else if(shaftCode.equals("z")) {
-                        sx1 = (widthTop - widthNow + longK)*-1;
-                        sz1 = 1;
-                    }
-
-                    if(shaftCode.equals("x")) {
-                        sz = (widthTop - widthNow + longK);
-                    }else if(shaftCode.equals("z")) {
-                        sx = (widthTop - widthNow + longK);
-                    }
-                }
-
-                Transformation transformation1 = blockDisplay1.getTransformation();
-
-                world.loadChunk(blockDisplay.getChunk());
-                world.loadChunk(blockDisplay1.getChunk());
-                world.loadChunk(new Location(world, x, height, z).getChunk());
-                world.loadChunk(new Location(world, x1, height, z1).getChunk());
-
-                blockDisplay.teleportAsync(new Location(world, x, height, z));
-                blockDisplay1.teleportAsync(new Location(world, x1, height, z1));
-
-                transformation.getScale().set(sx,400,sz);
-                blockDisplay.setTransformation(transformation);
-
-                transformation1.getScale().set(sx1,400,sz1);
-                blockDisplay1.setTransformation(transformation1);
-
             }else {
-                BlockDisplay blockDisplay = world.spawn(new Location(world,0,0,0),BlockDisplay.class);
-                blockDisplay.setBlock(Bukkit.createBlockData(Material.RED_STAINED_GLASS));
+                List<BlockDisplay> blockDisplays = new ArrayList<>();
+                List<BlockDisplay> blockDisplays1 = new ArrayList<>();
+                for (int j = 0; j < 10; j++){
+                    new Location(world,0,0,0).getChunk().addPluginChunkTicket(plugin);
+                    BlockDisplay blockDisplay = world.spawn(new Location(world,0,0,0),BlockDisplay.class);
+                    blockDisplay.setBlock(Bukkit.createBlockData(Material.RED_STAINED_GLASS));
 
-                if(blockDisplays.size()-1 > i*2) {
-                    blockDisplays.set(i * 2, blockDisplay);
-                }else {
-                    blockDisplays.add(blockDisplay);
+                    blockDisplays.add(j, blockDisplay);
+
+                    widthNow = widthNow+longK;
+
+                    BlockDisplay blockDisplay1 = world.spawn(new Location(world,0,0,0),BlockDisplay.class);
+                    blockDisplay1.setBlock(Bukkit.createBlockData(Material.RED_STAINED_GLASS));
+
+                    blockDisplays1.add(j, blockDisplay1);
+
                 }
-
-                widthNow = widthNow+longK;
-
-                BlockDisplay blockDisplay1 = world.spawn(new Location(world,0,0,0),BlockDisplay.class);
-                blockDisplay1.setBlock(Bukkit.createBlockData(Material.RED_STAINED_GLASS));
-
-                if(blockDisplays.size()-1 > i*2+1) {
-                    blockDisplays.set(i * 2 + 1, blockDisplay1);
-                }else {
-                    blockDisplays.add(blockDisplay1);
-                }
+                blockDisplaysList.add(blockDisplays);
+                blockDisplaysList1.add(blockDisplays1);
             }
         }
 
-        if(blockDisplays.size() > i*2) {
-            for(int ii = i+1; ii < blockDisplays.size(); ii++) {
-                blockDisplays.get(ii).setBlock(Bukkit.createBlockData(Material.EMERALD_BLOCK));
-                blockDisplays.get(ii).remove();
-                blockDisplays.remove(ii);
-            }
+        if(blockDisplaysList.size() > i) {
+
         }
 
 
